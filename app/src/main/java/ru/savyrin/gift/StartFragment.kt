7db4194fd0a2dc703ea2713a3b0e852abc5b2b94
SaVyrin <sarajkinaa@gmail.com>
2022-07-28 11:20:47 +0300

    Add fonts Change start screen appearance

--- app/src/main/java/ru/savyrin/gift/StartFragment.kt
+++ app/src/main/java/ru/savyrin/gift/StartFragment.kt
@@ -5,13 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import ru.savyrin.gift.databinding.FragmentStartBinding
 
 class StartFragment : Fragment() {
+
+    private var _binding: FragmentStartBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_start, container, false)
+    ): View {
+        _binding = FragmentStartBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setScreenClickListener()
+    }
+
+    private fun setScreenClickListener() {
+        binding.root.setOnClickListener {
+            // TODO navigate to next screen
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
