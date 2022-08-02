779d14be108be624f821207b5595c3cfc19dd9ea
SaVyrin <sarajkinaa@gmail.com>
2022-08-02 11:27:29 +0300

    Add start screen text animation

--- app/src/main/java/ru/savyrin/gift/ui/start/StartFragment.kt
+++ app/src/main/java/ru/savyrin/gift/ui/start/StartFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AlphaAnimation
+import android.view.animation.Animation
 import androidx.navigation.fragment.findNavController
 import ru.savyrin.gift.R
 import ru.savyrin.gift.databinding.FragmentStartBinding
@@ -25,12 +27,22 @@ class StartFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setScreenClickListener()
+        setTextFadingAnimation()
     }
 
     private fun setScreenClickListener() {
         binding.root.setOnClickListener {
             findNavController().navigate(R.id.action_startFragment_to_gameFragment)
         }
+    }
+
+    private fun setTextFadingAnimation() {
+        // TODO перенести это в di
+        val textAnimation = AlphaAnimation(1.0f, 0.2f)
+        textAnimation.duration = 1800
+        textAnimation.repeatCount = Animation.INFINITE
+        textAnimation.repeatMode = Animation.REVERSE
+        binding.hintTv.startAnimation(textAnimation)
     }
 
     override fun onDestroy() {
