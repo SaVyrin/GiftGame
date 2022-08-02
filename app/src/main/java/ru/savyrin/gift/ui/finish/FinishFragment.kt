19debf2224b7800f8347aaaadbd8205f818fc4ff
SaVyrin <sarajkinaa@gmail.com>
2022-08-02 16:39:54 +0300

    Add finish screen animation

--- app/src/main/java/ru/savyrin/gift/ui/finish/FinishFragment.kt
+++ app/src/main/java/ru/savyrin/gift/ui/finish/FinishFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AlphaAnimation
+import android.view.animation.Animation
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -56,7 +58,24 @@ class FinishFragment : Fragment() {
 
     private fun showNextGreetingText() {
         val nextGreetingText = viewModel.getNextGreeting()
-        binding.greetingsTv.text = nextGreetingText
+        animateTextChange(nextGreetingText)
+    }
+
+    private fun animateTextChange(nextGreetingText: String) {
+        // TODO добавить в di
+        val textChangeAnimation = AlphaAnimation(1.0f, 0.0f)
+        textChangeAnimation.duration = 500
+        textChangeAnimation.repeatCount = 1
+        textChangeAnimation.repeatMode = Animation.REVERSE
+
+        textChangeAnimation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationEnd(animation: Animation?) {}
+            override fun onAnimationStart(animation: Animation?) {}
+            override fun onAnimationRepeat(animation: Animation?) {
+                binding.greetingsTv.text = nextGreetingText
+            }
+        })
+        binding.greetingsTv.startAnimation(textChangeAnimation)
     }
 
     private fun setScreenClickListener() {
